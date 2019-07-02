2a347bca4f68e79b74526c5cdf35f2e118dedcf7
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2019-07-02 16:11:41 +0100

    Update main.py
    Application can now detect faces. It surround a blue rectangle around the face.

--- main.py
+++ main.py
@@ -1,15 +1,37 @@
 import numpy as np
 import cv2
 
+face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
+
 # Accesses the device's camera
 cap = cv2.VideoCapture(0)
 
-while(True):
+while True:
     # Capture frame-by-frame
     ret, frame = cap.read()
+    # Convert captured frame from RGB to greyscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+
+    # Prints a set of 4 values if a face is detected
+    for (x, y, w, h) in faces:
+        # roi = region of interest
+        print(x, y, w, h)
+        # (ycord_start, ycord_end)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        # img_item = "my-image.png"
+        # cv2.imwrite(img_item, roi_gray)
+        # BGR
+        color = (255, 0, 0)
+        stroke = 2
+        end_cord_x = x + w
+        end_cord_y = y + h
+        # Draws the rectangle around the face
+        cv2.rectangle(frame, (x, y), (end_cord_x, end_cord_y), color, stroke)
 
     # Show the frame
-    cv2.imshow('frame',frame)
+    cv2.imshow('frame', frame)
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
 
