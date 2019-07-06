11a9a1736e995ac0c8295306a2a3d0ac62e6c52e
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2019-07-06 16:31:15 +0100

    Update mobile.py
    Finished writing mobile.py

--- mobile.py
+++ mobile.py
@@ -1,12 +1,38 @@
 import kivy
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.uix.camera import Camera
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
 
 
-class MyApp(App):
+class CameraExample(App):
     def build(self):
-        return Label(text="Hello World")
+        layout = BoxLayout(orientation='vertical')
+
+        # Create camera object
+        self.cameraObject = Camera(play=False)
+        self.cameraObject.play = True
+        self.cameraObject.resolution = (300, 300)
+
+        # Button that captures pictures
+        self.cameraClick = Button(text="Take Photo")
+        self.cameraClick.size_hint(.5, .2)
+        self.cameraClick.pos_hint = {'x': .25, 'y':.75}
+
+        # Bind the button's on_press to onCameraClick
+        self.cameraClick.bind(on_press=self.onCameraClick)
+
+        # Append camera and button to the layout
+        layout.add_widget(self.cameraObject)
+        layout.add_widget(self.cameraClick)
+
+        # return the root widget
+        return layout
+
+    # Take the current frame of the video as the photograph
+    def onCameraClick(self, *args):
+        self.cameraObject.export_to_png('/kivyexamples/selfie.png')
 
 
-if __name__ == "__main__":
-    MyApp().run()
+if __name__ == '__main__':
+    CameraExample().run()
