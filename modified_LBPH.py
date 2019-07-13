cdfc27273829fa58de5166e6e605e6b979ba774f
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2019-07-13 14:59:10 +0100

    Update modified_LBPH.py
    This Python class represents a modified version of the Local Binary Pattern Histogram (LBPH) algorithm

---
+++ modified_LBPH.py
@@ -0,0 +1,13 @@
+class ModifiedLBPH:
+
+    def __init__(self, radius, neighbors, grid_x, grid_y):
+        '''
+        :param radius: Builds the circular local binary pattern and represents the radius around the central pixel (usually set to 1).
+        :param neighbors: Number of sample points to build the circular local binary pattern (usually set to 8).
+        :param grid_x: Number of cells in the horizontal direction (usually set to 8).
+        :param grid_y: Number of cells in the vertical direction (usually set to 8).
+        '''
+        self.radius = radius
+        self.neighbors = neighbors
+        self.grid_x = grid_x
+        self.grid_y = grid_y
