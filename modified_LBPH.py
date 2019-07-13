dc4d7f51551ab633d163a812127ccd6d2d54dfa7
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2019-07-13 15:22:20 +0100

    Update modified_LBPH.py
    Providing each parameter with default values

--- modified_LBPH.py
+++ modified_LBPH.py
@@ -1,6 +1,7 @@
 class ModifiedLBPH:
+    #
 
-    def __init__(self, radius, neighbors, grid_x, grid_y):
+    def __init__(self, radius=1, neighbors=8, grid_x=8, grid_y=8):
         '''
         :param radius: Builds the circular local binary pattern and represents the radius around the central pixel (usually set to 1).
         :param neighbors: Number of sample points to build the circular local binary pattern (usually set to 8).
@@ -11,3 +12,4 @@ class ModifiedLBPH:
         self.neighbors = neighbors
         self.grid_x = grid_x
         self.grid_y = grid_y
+
