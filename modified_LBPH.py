85481c4309e216e3a0f540cec05766941a4ed9d3
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2019-07-21 09:19:52 +0100

    Update modified_LBPH.py
    - Concatenating the binary digits into one string and converting that string into decimal.

--- modified_LBPH.py
+++ modified_LBPH.py
@@ -21,25 +21,22 @@ class ModifiedLBPH:
     def binary_matrix_conversion(self, three_by_three_matrix, threshold):
         # Turns the 3x3 matrix into a single list
         flattened_3_x_3_matrix = [item for sublist in three_by_three_matrix for item in sublist]
+        flattened_3_x_3_matrix.pop(4)
 
         binary_matrix = []
 
         # Creates the binary matrix
         for neighbour in flattened_3_x_3_matrix:
-            # If the position of the current neighbour is the same as the one in the threshold
-            if flattened_3_x_3_matrix.index(neighbour) == flattened_3_x_3_matrix.index(threshold):
-                # then make the central value into an empty space
-                pass
-            if threshold <= neighbour:
-                # Add a 1 to the binary matrix if the threshold is bigger than the current neighbour
+            # Add a 1 to the binary matrix if the neighbour is bigger than the threshold
+            if neighbour >= threshold:
                 binary_matrix.append(1)
-            elif threshold > neighbour:
-                # Add a 0 to the binary matrix if the threshold is bigger than the current neighbour
+            # Add a 0 to the binary matrix if the neighbour is smaller than the threshold
+            elif neighbour < threshold:
                 binary_matrix.append(0)
 
         lst = []
-        for index in range(0, len(binary_matrix), 3):
-            lst.append(binary_matrix[index:index + 3])
+        for index in range(0, len(binary_matrix), 4):
+            lst.append(binary_matrix[index:index + 4])
 
         binary_matrix = np.array(lst)
 
@@ -48,29 +45,25 @@ class ModifiedLBPH:
     def add_image(self, image):
 
         row = 0
-        col = 0
+        column = 0
 
         while row + 3 < len(image[0]):
-            while col + 3 < len(image[0]):
-                grey_sample = image[row:row+3, col:col+3]
+            while column + 3 < len(image[0]):
+                grey_sample = image[row:row+3, column:column+3]
                 # print(grey_sample)
                 # Central value of the matrix
-                threshold = image[row+1, col+1]
+                threshold = image[row+1, column+1]
                 # Converting the 3x3 matrix into a binary 3x3 matrix
                 b = self.binary_matrix_conversion(grey_sample, threshold)
                 print(b)
-                # self.binary_matrix.append(self.binary_matrix_conversion(grey_sample, threshold))
+
+                # Concatenate the binary values into one string
+                binary_values = [str(digit) for sublist in b for digit in sublist]
+                binary_values = "".join(binary_values)
+
                 # Converts the binary number into a decimal one
-                # decimal_num = binary_to_dec(binary_matrix)
-                col += 3
+                decimal_num = int(binary_values, 2)
+                print(decimal_num)
+                column += 3
             print("\n")
             row += 3
-
-    # Finds the decimal equivalent of the binary number
-    def binary_to_dec(self, binary_number):
-        binary = ""
-        for digit in binary_number:
-            binary += str(digit)
-
-        # Converts binary into its decimal equivalent
-        return int(binary, 2)
