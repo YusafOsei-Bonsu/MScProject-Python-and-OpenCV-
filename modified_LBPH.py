030f8adbf9a3d13c24bb79160d4b5084172bdafb
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2019-07-28 15:43:02 +0100

    Update modified_LBPH.py
    Created histogram matching method inside modified lbph class

--- modified_LBPH.py
+++ modified_LBPH.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 # 'feature' contains the implementation of the LBP descriptor
 from skimage import feature
@@ -10,6 +11,7 @@ class ModifiedLBPH:
         :param radius: Builds the circular local binary pattern and represents the radius around the central pixel (usually set to 1).
         :param neighbors: Number of sample points to build the circular local binary pattern (usually set to 8).
         '''
+        self.histograms = None
         self.radius = radius
         self.neighbors = neighbors
         self.labels = {}
@@ -36,8 +38,29 @@ class ModifiedLBPH:
         self.add_label(label)
         histogram = self.lbp_process(roi)
         histograms = self.labels[label]
-        print(self.labels['yusaf'])
+        # print(self.labels['yusaf'])
         histograms.append(histogram)
 
+    # The histogram closest to the target img is the match.
+    def histogram_matching(self, image):
+        # Computing histogram from given image
+        histogram = self.lbp_process(image)
+        l = 0
+        m = 999
 
+        for j in len(self.labels):
+            for k in len(j):
+                d = self.distance(histogram, self.labels[j][k])
+                if d < m:
+                    l = j
+                    m = d
+        return l
 
+    # Calculating the distance between two histograms
+    def distance(self, histogram_1, histogram_2):
+        sum = 0
+
+        for i in range(1, 1000):
+            sum += ((histogram_1[i] - histogram_2[i]) ** 2)
+
+        return math.sqrt(sum)
