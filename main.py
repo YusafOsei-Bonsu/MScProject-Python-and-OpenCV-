500c1c944d97fe42e0cf69a31a5bdd9894bdfa17
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2019-08-22 15:08:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ base_directory = os.path.dirname(os.path.abspath(__file__))
 # The directory containing all test images (that originate from LFW DB)
 LFW_images = os.path.join(base_directory, "LFW-images")
 
-face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
 
 img_array = None
 resulting_label = None
@@ -22,17 +22,13 @@ for root, directories, files in os.walk(LFW_images):
     for file in files:
         if (file.endswith("png") or file.endswith("jpg")) and "001" in file:
             path = os.path.join(root, file)
-            # label = os.path.basename(os.path.dirname(path))
             label = file[:3]
-            #print(label)
             img = cv2.imread(path)
 
             # Converting the image into greyscale
             pil_image = Image.open(path).convert("L")
-            #
             img_array = np.array(pil_image, "uint8")
             faces = face_cascade.detectMultiScale(img_array, scaleFactor=1.5, minNeighbors=5)
-            #
             for (x, y, w, h) in faces:
                 region_of_interest = img_array[y:y + h, x:x + w]
                 # Training data
@@ -53,7 +49,6 @@ for root, directories, files in os.walk(LFW_images):
 
             # Converting the image into greyscale
             pil_image = Image.open(path).convert("L")
-            #
             img_array = np.array(pil_image, "uint8")
             faces = face_cascade.detectMultiScale(img_array, scaleFactor=1.5, minNeighbors=5)
 
@@ -61,8 +56,8 @@ for root, directories, files in os.walk(LFW_images):
             for (x, y, w, h) in faces:
                 region_of_interest = img_array[y:y + h, x:x + w]
                 label = testing_lbph.histogram_matching(region_of_interest)
-                print(file, label)
                 if label == file[:3]:
+                    print(file, label)
                     correct += 1
 
 acc = correct / float(count) * 100.0
